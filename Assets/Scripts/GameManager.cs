5907bed930c7410e99c3369531154829523b2643
Aedan-Vongsouthi-Student <aevo910@student.oaisd.org>
2023-04-10 15:55:19 -0400

    bklrn

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    Quiz quiz;
+    EndScreen endScreen;
+
+    void Start()
+    {
+        quiz = FindObjectOfType<Quiz>();
+        endScreen = FindObjectOfType<EndScreen>();
+
+        quiz.gameObject.SetActive(true);
+        endScreen.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (quiz.isComplete)
+        {
+            quiz.gameObject.SetActive(false);
+            endScreen.gameObject.SetActive(true);
+        }
+    }
+
+    public void OnReplayLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
